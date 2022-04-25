ebb5a7f8fabec0fb8f0d7eb169d85b1071c6be6c
Alice <alexandervtarasovddd@gmail.com>
2022-04-25 17:32:59 +0300

    some conf changes

--- src/main/scala/own/LocalSystemStart.scala
+++ src/main/scala/own/LocalSystemStart.scala
@@ -4,6 +4,6 @@ import akka.actor.ActorSystem
 import com.typesafe.config.ConfigFactory
 
 object LocalSystemStart extends App {
-  val config = ConfigFactory.load("host")
+  val config = ConfigFactory.load("host").getConfig("Local")
   val system = ActorSystem("local",config)
 }
